d06881ee4d1e5a599285fd0859e65bdacefebdba
omaro <omaroubari16@gmail.com>
2026-08-02 21:59:04 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  adapter: cloudflare()
+});
